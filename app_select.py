e282b4bbd38b298f8d03d4bc7cf447f500631eaf
Mario Morales <lmmoralesl@gmail.com>
2022-05-06 14:38:22 -0500

    create dataframe with returned list

--- app_select.py
+++ app_select.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import csv
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -18,7 +19,22 @@ def index():
         "ADMITIDOS": row[5],
         "ASPIRANTES": row[6]
       })
+
   return render_template("index.html", carreras=carreras)
+
+@app.route("/select_value", methods=['GET', 'POST'])
+def select_value():
+    seleccion = request.form.get('seleccion_carrera')
+    
+    #limpiar la lista de espacios en blanco y nuevas lineas
+    seleccion_nnl = seleccion.replace('\r\n','')
+    selection_clean = seleccion_nnl.replace(' ','')
+    seleccion_list = selection_clean.split(',')
+    
+    input_variables = pd.DataFrame(seleccion_list)
+    print(input_variables)
+    
+    return (seleccion)
 
 if __name__ == "__main__":
     app.run(debug=True)
