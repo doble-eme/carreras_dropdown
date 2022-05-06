27aa19f82340d1a21035fa08758c0f763710898e
Mario Morales <lmmoralesl@gmail.com>
2022-05-06 16:16:47 -0500

    add home button to reload

--- app_select.py
+++ app_select.py
@@ -1,44 +1,48 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 import pickle
 import pandas as pd
 import csv
 
 #Initialize Flask app
-app = Flask(__name__) 
+app = Flask(__name__)
+
 #Loads the trained model
 model = pickle.load(open('modelo_udg.pkl', 'rb'))
 
-@app.route("/") # Homepage and data read
+@app.route("/", methods = ['GET', 'POST']) # Homepage and data read
 def index():
-  with open('.data/PuntajesMinimos_last.csv') as csv_file:
-    data = csv.reader(csv_file, delimiter=',')
-    carreras = []
-    for row in data:
-      carreras.append({
-        "CALENDARIO": row[0],
-        "CENTRO": row[1],
-        "CARRERA": row[2],
-        "PUNTAJE_MINIMO": row[3],
-        "CUPO": row[4],
-        "ADMITIDOS": row[5],
-        "ASPIRANTES": row[6]
-      })
+    '''
+    Reads data from csv file
+    '''
+    with open('.data/PuntajesMinimos_last.csv') as csv_file:
+        data = csv.reader(csv_file, delimiter=',')
+        carreras = []
+        for row in data:
+            carreras.append({
+                "CALENDARIO": row[0],
+                "CENTRO": row[1],
+                "CARRERA": row[2],
+                "PUNTAJE_MINIMO": row[3],
+                "CUPO": row[4],
+                "ADMITIDOS": row[5],
+                "ASPIRANTES": row[6]
+            })
 
-  return render_template("index.html", carreras=carreras)
+    return render_template("index.html", carreras=carreras)
 
-@app.route("/select_value", methods=['GET', 'POST'])
+@app.route("/select_value", methods=['GET','POST'])
 def select_value():
     '''
     Reads the selection from the dropdown menu
     '''
     seleccion = request.form.get('seleccion_carrera')
-    
+        
     #Clean list to remove white spaces and new lines
     seleccion_nnl = seleccion.replace('\r\n','')
     selection_clean = seleccion_nnl.replace(' ','')
     seleccion_list = selection_clean.split(',')
-    
+        
     #Creates the data frame to be sent to the predicion model
     input_variables = pd.DataFrame(seleccion_list).T
 
@@ -53,7 +57,7 @@ def select_value():
         result = 'Probabilidad alta'
 
     # rendering the predicted result on the page
-    return render_template('index.html', prediction_text=result) 
+    return render_template('index.html', prediction_text=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
