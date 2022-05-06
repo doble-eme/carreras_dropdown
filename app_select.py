c476cf935b3fd08678e9bfa8cb10c2b6d9f5ed3d
Mario Morales <lmmoralesl@gmail.com>
2022-05-06 15:04:13 -0500

    Add predict functionality

--- app_select.py
+++ app_select.py
@@ -1,10 +1,15 @@
+import numpy as np
 from flask import Flask, request, jsonify, render_template
-import csv
+import pickle
 import pandas as pd
+import csv
 
-app = Flask(__name__)
+#Initialize Flask app
+app = Flask(__name__) 
+#Loads the trained model
+model = pickle.load(open('modelo_udg.pkl', 'rb'))
 
-@app.route("/")
+@app.route("/") # Homepage and data read
 def index():
   with open('.data/PuntajesMinimos_last.csv') as csv_file:
     data = csv.reader(csv_file, delimiter=',')
@@ -24,17 +29,31 @@ def index():
 
 @app.route("/select_value", methods=['GET', 'POST'])
 def select_value():
+    '''
+    Reads the selection from the dropdown menu
+    '''
     seleccion = request.form.get('seleccion_carrera')
     
-    #limpiar la lista de espacios en blanco y nuevas lineas
+    #Clean list to remove white spaces and new lines
     seleccion_nnl = seleccion.replace('\r\n','')
     selection_clean = seleccion_nnl.replace(' ','')
     seleccion_list = selection_clean.split(',')
     
-    input_variables = pd.DataFrame(seleccion_list)
-    print(input_variables)
-    
-    return (seleccion)
+    #Creates the data frame to be sent to the predicion model
+    input_variables = pd.DataFrame(seleccion_list).T
+
+    #Executes the prediction using the trained model
+    # print(input_variables)
+    prediction = model.predict(input_variables)[0]
+
+    #if statement to determine if the probability is high or low according to the prediction result
+    if prediction == 0:
+        result = 'Probabilidad baja'
+    elif prediction == 1:
+        result = 'Probabilidad alta'
+
+    # rendering the predicted result on the page
+    return render_template('index.html', prediction_text=result) 
 
 if __name__ == "__main__":
     app.run(debug=True)
