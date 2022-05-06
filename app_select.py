ae9246f755372ba3895bd079b0647e5b18b34cde
Mario Morales <lmmoralesl@gmail.com>
2022-05-06 11:36:04 -0500

    change structure of list

--- app_select.py
+++ app_select.py
@@ -7,21 +7,17 @@ app = Flask(__name__)
 def index():
   with open('.data/PuntajesMinimos_last.csv') as csv_file:
     data = csv.reader(csv_file, delimiter=',')
-    first_line = True
     carreras = []
     for row in data:
-      if not first_line:
-        carreras.append({
-          "CENTRO": row[0],
-          "CARRERA": row[1],
-          "ASPIRANTES": row[2],
-          "ADMITIDOS": row[3],
-          "CUPO": row[4],
-          "PUNTAJE_MINIMO": row[5],
-          "CALENDARIO": row[6]
-        })
-      else:
-        first_line = False
+      carreras.append({
+        "CALENDARIO": row[0],
+        "CENTRO": row[1],
+        "CARRERA": row[2],
+        "PUNTAJE_MINIMO": row[3],
+        "CUPO": row[4],
+        "ADMITIDOS": row[5],
+        "ASPIRANTES": row[6]
+      })
   return render_template("index.html", carreras=carreras)
 
 if __name__ == "__main__":
